47b3ec1ee5cf7e4eb2104be630f041dab99b19e3
S73ph4n <stephanpissot@gmail.com>
2021-09-18 16:26:04 +0200

    Fixed FileNotFoundError

--- sopel_papermill/__init__.py
+++ sopel_papermill/__init__.py
@@ -55,6 +55,7 @@ def papermill_execute(bot, trigger):
         pm.execute_notebook(path_in, path_out)
     except FileNotFoundError:
         bot.reply('That notebook does not exist.')
+        return
     bot.reply('Notebook ' + nb_name + ' execution ok.')
     if bot.config.papermill.show_last_line :
         bot.say('Last cell output:')
